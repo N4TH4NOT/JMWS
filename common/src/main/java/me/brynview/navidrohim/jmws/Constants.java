75bd1c1cbab07171f9b1aa9f59513410c4d6ad51
N4TH4NOT <n4th4not+git@gmail.com>
2025-09-06 09:36:05 +0200

    Update to MC 1.21.6 (Forge was unavailable)

--- common/src/main/java/me/brynview/navidrohim/jmws/Constants.java
+++ common/src/main/java/me/brynview/navidrohim/jmws/Constants.java
@@ -17,9 +17,9 @@ public class Constants {
 
     public static final String MODID = "jmws";
     public static final String MOD_NAME = "JMWS";
-    public static final String VERSION = "1.1.5-1.21.8";
+    public static final String VERSION = "1.1.5-1.21.6";
 
-    public static final List<String> forgeModLoaders = List.of("Forge", "NeoForge");
+    public static final List<String> forgeModLoaders = List.of("NeoForge");
     public static final List<String> forbiddenGroups = List.of("journeymap_death", "journeymap_all", "journeymap_temp", "journeymap_default");
-    public static final String JourneyMapVersionString = "1.21.7-6.0.0-beta.53";
+    public static final String JourneyMapVersionString = "1.21.6-6.0.0-beta.52";
 }
